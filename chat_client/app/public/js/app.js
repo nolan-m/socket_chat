40219a912471fec21ea8a006fe337e2f7024b158
nolan-m <namiller2@gmail.com>
2015-10-15 21:45:18 -0700

    Move to app controller to correct file

--- chat_client/app/public/js/app.js
+++ chat_client/app/public/js/app.js
@@ -1,28 +1,64 @@
 'use strict';
 
-angular.module('chat', [])
-  .factory('GUI', function() {
-    return require('nw.gui');
-  })
-  .factory('Window', ['GUI', function(gui) {
-    return gui.Window.get();
-  }])
-  .controller('Toolbar', ['$scope', 'Window',
-    function($scope, Window) {
-      /*
-      
-      Custom Control for Window operations
+//load angular
+var app = angular.module('chat', ['ngMaterial', 'ngAnimate', 'ngMdIcons', 'btford.socket-io']);
 
-      $scope.minimize = function() {
-        Window.minimize();
-      };
+//server url
+var serverBaseUrl = 'http://localhost:2015';
 
-      $scope.toggleFullscreen = function() {
-        Window.toggleKioskMode();
-      };
+//services to interact with nodewebkit GUI and Window
+app.factory('GUI', function () {
+  return require('nw.gui');
+});
+app.factory('Window', function (GUI) {
+  return GUI.Window.get();
+});
 
-      $scope.close = function() {
-        Window.close();
-      };*/
+//service to interact with the socket library
+app.factory('socket', function (socketFactory) {
+  var myIoSocket = io.connect(serverBaseUrl);
+  var socket = socketFactory({
+    ioSocket: myIoSocket
+  });
+
+  return socket;
+});
+
+//ng-enter directive
+app.directive('ngEnter', function () {
+  return function (scope, element, attrs) {
+    element.bind('keydown keypress', function (event) {
+      if (event.which === 13) {
+        scope.$apply(function() {
+          scope.$eval(attrs.ngEnter);
+        });
+
+        event.preventDefault();
+      }
+    });
+  };
+});
+
+app.controlller('MainCtrl', function ($scope, Window, GUI, $mdDialog, socket, $http) {
+
+  //menu setup
+  $scope.messages = [];
+  $scope.room   = '';
+
+  var windowMenu = new GUI.Menu({
+    type: 'menubar'
+  });
+  var roomsMenu = new GUI.Menu();
+
+  windowMenu.append(new GUI.MenuItem({
+    label: 'Rooms',
+    submenu: roomsMenu
+  }));
+
+  windowMenu.append(new GUI.MenuItem({
+    label: 'Exit',
+    click: function () {
+      Window.close();
     }
-  ]);
+  }));
+});
