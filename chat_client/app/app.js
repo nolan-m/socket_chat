e08d8cf9f2a4ebc40cdd4068c3610509bc32ba05
nolan-m <namiller2@gmail.com>
2015-10-15 19:51:51 -0700

    Bind event to enter key

--- chat_client/app/app.js
+++ chat_client/app/app.js
@@ -25,3 +25,42 @@ var server = app.listen(app.get('port'), function() {
 });
 
 module.exports = app;
+
+//load angular
+var app = angular.module('chat', ['ngMaterial', 'ngAnimate', 'ngMdIcons', 'btford.socket-io']);
+
+//server url
+var serverBaseUrl = 'http://localhost:2015';
+
+//services to interact with nodewebkit GUI and Window
+app.factory('GUI', function () {
+	return require('nw.gui');
+});
+app.factory('Window', function (GUI) {
+	return GUI.Window.get();
+});
+
+//service to interact with the socket library
+app.factory('socket', function (socketFactory) {
+	var myIoSocket = io.connect(serverBaseUrl);
+	var socket = socketFactory({
+		ioSocket: myIoSocket
+	});
+
+	return socket;
+});
+
+//ng-enter directive
+app.directive('ngEnter', function () {
+	return function (scope, element, attrs) {
+		element.bind('keydown keypress', function (event) {
+			if (event.which === 13) {
+				scope.$apply(function() {
+					scope.$eval(attrs.ngEnter);
+				});
+
+				event.preventDefault();
+			}
+		});
+	};
+});
