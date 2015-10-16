aece776150d213b7dccb0255ea205798ed5182cd
nolan-m <namiller2@gmail.com>
2015-10-15 21:51:06 -0700

    Add available rooms to the room menu

--- chat_client/app/public/js/app.js
+++ chat_client/app/public/js/app.js
@@ -8,57 +8,92 @@ var serverBaseUrl = 'http://localhost:2015';
 
 //services to interact with nodewebkit GUI and Window
 app.factory('GUI', function () {
-  return require('nw.gui');
+	return require('nw.gui');
 });
 app.factory('Window', function (GUI) {
-  return GUI.Window.get();
+	return GUI.Window.get();
 });
 
 //service to interact with the socket library
 app.factory('socket', function (socketFactory) {
-  var myIoSocket = io.connect(serverBaseUrl);
-  var socket = socketFactory({
-    ioSocket: myIoSocket
-  });
+	var myIoSocket = io.connect(serverBaseUrl);
+	var socket = socketFactory({
+		ioSocket: myIoSocket
+	});
 
-  return socket;
+	return socket;
 });
 
 //ng-enter directive
 app.directive('ngEnter', function () {
-  return function (scope, element, attrs) {
-    element.bind('keydown keypress', function (event) {
-      if (event.which === 13) {
-        scope.$apply(function() {
-          scope.$eval(attrs.ngEnter);
-        });
+	return function (scope, element, attrs) {
+		element.bind('keydown keypress', function (event) {
+			if (event.which === 13) {
+				scope.$apply(function() {
+					scope.$eval(attrs.ngEnter);
+				});
 
-        event.preventDefault();
-      }
-    });
-  };
+				event.preventDefault();
+			}
+		});
+	};
 });
 
 app.controlller('MainCtrl', function ($scope, Window, GUI, $mdDialog, socket, $http) {
 
-  //menu setup
-  $scope.messages = [];
-  $scope.room   = '';
+	//menu setup
+	$scope.messages = [];
+	$scope.room   = '';
 
-  var windowMenu = new GUI.Menu({
-    type: 'menubar'
-  });
-  var roomsMenu = new GUI.Menu();
+	var windowMenu = new GUI.Menu({
+		type: 'menubar'
+	});
+	var roomsMenu = new GUI.Menu();
 
-  windowMenu.append(new GUI.MenuItem({
-    label: 'Rooms',
-    submenu: roomsMenu
-  }));
+	windowMenu.append(new GUI.MenuItem({
+		label: 'Rooms',
+		submenu: roomsMenu
+	}));
 
-  windowMenu.append(new GUI.MenuItem({
-    label: 'Exit',
-    click: function () {
-      Window.close();
-    }
-  }));
+	windowMenu.append(new GUI.MenuItem({
+		label: 'Exit',
+		click: function () {
+			Window.close();
+		}
+	}));
+
+	//listen for setup event and create rooms
+	socket.on('setup', function (data) {
+		var rooms = data.rooms;
+
+		for (var r = 0; r < rooms.length; r++) {
+			//loop and appeand room to the window room menu
+			handleRoomSubMenu(r);
+		}
+
+		//handle creation of room
+		function handleRoomSubMenu(r) {
+			var clickedRoom = rooms[r];
+			//append each room to menu
+			roomsMenu.append(new GUI.MenuItem({
+				label: clickedRoom.toUpperCase()
+				click: function () {
+					$scope.room = clickedRoom.toUpperCase();
+
+					//notify the server that the user changed their room
+					socket.emit('switch room', {
+						newRoom: clickedRoom,
+						username: $scope.username
+					});
+
+					$http.get(serverBaseUrl + '/msg?room=' + clickedRoom).success(function(msgs) {
+						$scope.messages = msgs;
+					});
+				}
+			}));
+		}
+		//Attach menu
+		GUI.Window.get().menu = windowMenu;
+	});
+
 });
